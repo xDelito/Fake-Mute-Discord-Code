72ac5a8b1f1b40eaa67085f7b3ebc5f2f6662a41
xDelito <89098263+xDelito@users.noreply.github.com>
2022-02-22 14:04:00 +0100

    Add files via upload

---
+++ FakeMuteDiscordCode.js
@@ -0,0 +1,19 @@
+var text = new TextDecoder("utf-8");
+
+WebSocket.prototype.original = WebSocket.prototype.send;
+WebSocket.prototype.send = function(data) {
+    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+        if (text.decode(data).includes("self_deaf")) {
+            console.log("found mute/deafen");
+            data = data.replace('"self_mute":false', 'discord.gg/security');
+            console.log("Faked");
+        }
+    }
+    WebSocket.prototype.original.apply(this, [data]);
+}
+
+
+    stop() {
+
+    }
+}
